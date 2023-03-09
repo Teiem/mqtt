7eb0d344d3faa182b09c4e79f099f8624ee4f3f6
Tim <tim@londschien.com>
2023-03-09 11:59:10 +0100

    format data.ts

--- src/lib/data.ts
+++ src/lib/data.ts
@@ -11,11 +11,11 @@ export class TypedDexie extends Dexie {
     messages!: Table<Message>;
 
     constructor() {
-      super('myDatabase');
-      this.version(1).stores({
-        messages: '++id, topic, message, timestamp',
-      });
+        super('myDatabase');
+        this.version(1).stores({
+            messages: '++id, topic, message, timestamp',
+        });
     }
-  }
+}
 
 export const db = new TypedDexie();
